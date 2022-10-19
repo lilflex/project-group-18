d76318f80897ef56ae3436a84e5198878baf1992
Aleksej <kralinaleksej@gmail.com>
2022-10-19 21:21:51 +0300

    js

--- src/js/mobile-menu.js
+++ src/js/mobile-menu.js
@@ -2,13 +2,17 @@
   const mobileMenu = document.querySelector('.js-menu-container');
   const openMenuBtn = document.querySelector('.js-open-menu');
   const closeMenuBtn = document.querySelector('.js-close-menu');
+  const nav = document.querySelectorAll('.mobile-menu__link');
 
   const toggleMenu = () => {
-    const isMenuOpen = openMenuBtn.getAttribute('aria-expanded') === 'true' || false;
+    const isMenuOpen =
+      openMenuBtn.getAttribute('aria-expanded') === 'true' || false;
     openMenuBtn.setAttribute('aria-expanded', !isMenuOpen);
     mobileMenu.classList.toggle('is-open');
 
-    const scrollLockMethod = !isMenuOpen ? 'disableBodyScroll' : 'enableBodyScroll';
+    const scrollLockMethod = !isMenuOpen
+      ? 'disableBodyScroll'
+      : 'enableBodyScroll';
     bodyScrollLock[scrollLockMethod](document.body);
   };
 
@@ -22,4 +26,10 @@
     openMenuBtn.setAttribute('aria-expanded', false);
     bodyScrollLock.enableBodyScroll(document.body);
   });
+
+  for (var i = 0; i < nav.length; i++) {
+    nav[i].onclick = function (e) {
+      mobileMenu.classList.remove('is-open');
+    };
+  }
 })();
--- src/js/modal.js
+++ src/js/modal.js
@@ -1,70 +1,39 @@
-!function(e){"function"!=typeof e.matches&&(e.matches=e.msMatchesSelector||e.mozMatchesSelector||e.webkitMatchesSelector||function(e){for(var t=this,o=(t.document||t.ownerDocument).querySelectorAll(e),n=0;o[n]&&o[n]!==t;)++n;return Boolean(o[n])}),"function"!=typeof e.closest&&(e.closest=function(e){for(var t=this;t&&1===t.nodeType;){if(t.matches(e))return t;t=t.parentNode}return null})}(window.Element.prototype);
+// Get the button that opens the modal
+var btn = document.querySelectorAll('button.modal-open-button');
 
+var iframe = document.getElementById('youtube');
+// All page modals
+var modals = document.querySelectorAll('.backdrop');
 
-document.addEventListener('DOMContentLoaded', function() {
+// Get the <span> element that closes the modal
+var spans = document.getElementsByClassName('modal__close');
 
-   /* Записываем в переменные массив элементов-кнопок и подложку.
-      Подложке зададим id, чтобы не влиять на другие элементы с классом overlay*/
-   var modalButtons = document.querySelectorAll('.js-open-modal'),
-       overlay      = document.querySelector('.js-overlay-modal'),
-       closeButtons = document.querySelectorAll('.js-modal-close');
+// When the user clicks the button, open the modal
+for (var i = 0; i < btn.length; i++) {
+  btn[i].onclick = function (e) {
+    e.preventDefault();
+    iframe.setAttribute('src', 'https://www.youtube.com/embed/xrbKIyBFAFM');
+    modals = document.querySelector(e.target.getAttribute('modal'));
+    modals.classList.remove('is-hidden');
+    document.body.classList.add('no-scroll');
+  };
+}
 
+// When the user clicks on <span> (x), close the modal
+for (var i = 0; i < spans.length; i++) {
+  spans[i].onclick = function () {
+    if (typeof modals.classList.contains('is-hidden') !== !false)
+      modals.classList.add('is-hidden');
+    document.body.classList.remove('no-scroll');
+    iframe.removeAttribute('src', 'https://www.youtube.com/embed/xrbKIyBFAFM');
+  };
+}
 
-   /* Перебираем массив кнопок */
-   modalButtons.forEach(function(item){
-
-      /* Назначаем каждой кнопке обработчик клика */
-      item.addEventListener('click', function(e) {
-
-         /* Предотвращаем стандартное действие элемента. Так как кнопку разные
-            люди могут сделать по-разному. Кто-то сделает ссылку, кто-то кнопку.
-            Нужно подстраховаться. */
-         e.preventDefault();
-
-         /* При каждом клике на кнопку мы будем забирать содержимое атрибута data-modal
-            и будем искать модальное окно с таким же атрибутом. */
-         var modalId = this.getAttribute('data-modal'),
-             modalElem = document.querySelector('.modal[data-modal="' + modalId + '"]');
-
-
-         /* После того как нашли нужное модальное окно, добавим классы
-            подложке и окну чтобы показать их. */
-         modalElem.classList.add('active');
-         overlay.classList.add('active');
-      }); // end click
-
-   }); // end foreach
-
-
-   closeButtons.forEach(function(item){
-
-      item.addEventListener('click', function(e) {
-         var parentModal = this.closest('.modal');
-
-         parentModal.classList.remove('active');
-         overlay.classList.remove('active');
-      });
-
-   }); // end foreach
-
-
-    document.body.addEventListener('keyup', function (e) {
-        var key = e.keyCode;
-
-        if (key == 27) {
-
-            document.querySelector('.modal.active').classList.remove('active');
-            document.querySelector('.overlay').classList.remove('active');
-        };
-    }, false);
-
-
-    overlay.addEventListener('click', function() {
-        document.querySelector('.modal.active').classList.remove('active');
-        this.classList.remove('active');
-    });
-
-
-
-
-}); // end ready
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function (event) {
+  if (event.target == modals) {
+    modals.classList.add('is-hidden');
+    document.body.classList.remove('no-scroll');
+    iframe.removeAttribute('src', 'https://www.youtube.com/embed/xrbKIyBFAFM');
+  }
+};
